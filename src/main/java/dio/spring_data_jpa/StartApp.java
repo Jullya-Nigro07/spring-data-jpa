2db4bb1d7ccbbdbbc3e0deff1097f98886190eca
Jullya Nigro <nigrojullya07@gmail.com>
2026-01-05 19:01:29 -0300

    Add files via upload

---
+++ src/main/java/dio/spring_data_jpa/StartApp.java
@@ -0,0 +1,29 @@
+package dio.spring_data_jpa;
+
+
+import dio.spring_data_jpa.model.User;
+import dio.spring_data_jpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartApp implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception{
+        User user = new User();
+        user.setName("Fernando");
+        user.setUsername("frnd");
+        user.setPassword("teste123");
+
+        repository.save(user);
+
+        for(User u: repository.findAll()){
+            System.out.println(u);
+        }
+    }
+}
