3fc89420bfa37eda8ec3c2a1b0b97338e11e1ef3
Jullya-Nigro07 <nigrojullya07@gmail.com>
2026-01-05 23:46:08 -0300

    feat: Query Method e Query Override

--- src/main/java/dio/spring_data_jpa/StartApp.java
+++ src/main/java/dio/spring_data_jpa/StartApp.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
 
@@ -15,10 +17,27 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception{
+
+        List<User> users = repository.filtrarPorNome("Jullya");
+        List<User> users1 = repository.findByNameContaining("Fernando");
+        User users3 = repository.findByUsername("sml2");
+
+        for (User u : users){
+            System.out.println(u);
+        }
+
+        for (User u : users1){
+            System.out.println(u);
+        }
+
+        System.out.println(users3);
+    }
+
+    private void insertUsers(){
         User user = new User();
-        user.setName("Fernando");
-        user.setUsername("frnd");
-        user.setPassword("teste123");
+        user.setName("Peter");
+        user.setUsername("ptrr40");
+        user.setPassword("teste234");
 
         repository.save(user);
 
